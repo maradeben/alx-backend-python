aaeb966fd2db6d69bba484577cff7ed5b54abcb4
Eben <maradebenezer@gmail.com>
2023-10-11 08:08:35 +0100

    fixing 1

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -9,5 +9,5 @@ async def async_generator() -> Generator[float, None, float]:
     """ yield 10 random numbers async """
 
     for i in range(10):
-        await asyncio.sleep(1)
         yield random.uniform(0, 10)
+        await asyncio.sleep(1)
