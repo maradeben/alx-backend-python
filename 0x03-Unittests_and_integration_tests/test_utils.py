9452e0eb61721f08950fcb02d4729a0d2250bc50
Eben <maradebenezer@gmail.com>
2023-10-28 11:00:27 +0100

    solved 1

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,17 +3,26 @@
 import unittest
 from parameterized import parameterized
 from utils import access_nested_map
+from typing import Dict, Tuple
 
 
 class TestAccessNestedMap(unittest.TestCase):
     """ tests for access_nested_map """
-    
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-    def test_access_nested_map(self, nested_map, path, output):
+    def test_access_nested_map(self, nested_map: Dict,
+                               path: Tuple[str], output: int) -> None:
         """ test case """
         self.assertEqual(access_nested_map(nested_map, path), output)
 
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map: Dict,
+                                         path: Tuple[str]) -> None:
+        """ test case with KeyError """
+        self.assertRaises(KeyError, access_nested_map, nested_map, path)
